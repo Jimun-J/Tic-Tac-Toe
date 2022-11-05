765fe404b60388f6859a203841f5b4c551d5cfef
Jimun Jang <wkdwlans009@gmail.com>
2022-11-04 19:52:34 -0700

    Add gitignore procfile

--- app.js
+++ app.js
@@ -13,6 +13,11 @@ app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, "/public")));
 app.use(bodyParser.urlencoded({extended: true}));
 
+let port = process.env.PORT;
+if (port == null || port =="") {
+    port = 3000;
+}
+
 let marker = '';
 let rooms = [];
 let users = [];
@@ -142,6 +147,6 @@ io.on('connection', (socket) => {
     });
 });
 
-server.listen(3000, () => {
+server.listen(port, () => {
     console.log('server is running on port 3000');
 });
